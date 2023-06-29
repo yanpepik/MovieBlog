de9ca995a15e737236166c5d15ef261f0e6571b4
yanpepik <56980028+yanpepik@users.noreply.github.com>
2023-06-29 13:11:10 +0200

    Initial commit

---
+++ MovieBlog/MovieBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBlogApp.swift
+//  MovieBlog
+//
+//  Created by Yan Pepik on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
